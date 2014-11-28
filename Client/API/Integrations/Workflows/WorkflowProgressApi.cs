d7ff6103dbd90e0d5c720dd853ece24dd23db710
John McKim <johncmckim@gmail.com>
2014-11-28 11:46:52 +1000

    Update api to match new URL

--- Client/API/Integrations/Workflows/WorkflowProgressApi.cs
+++ Client/API/Integrations/Workflows/WorkflowProgressApi.cs
@@ -8,15 +8,20 @@ using System.Threading.Tasks;
 namespace CareerHub.Client.API.Integrations.Workflows {
     internal class WorkflowProgressApi : IWorkflowProgressApi {
 
-        private const string ApiBase = "api/integrations/v1/workflows/progress";
+        private const string ApiBase = "api/integrations/v1/workflows";
         private readonly OAuthHttpClient client = null;
 
         public WorkflowProgressApi(string baseUrl, string accessToken) {
             client = new OAuthHttpClient(baseUrl, ApiBase, accessToken);
 		}
 
-        public Task<IEnumerable<ProgressModel>> Get(int id) {
-            return client.GetResource<IEnumerable<ProgressModel>>(id.ToString());
+        public Task<IEnumerable<ProgressModel>> Get(int workflowId) {
+            string resource = GetResource(workflowId);
+            return client.GetResource<IEnumerable<ProgressModel>>(resource);
+        }
+
+        private string GetResource(int workflowId, string resource = "") {
+            return String.Format("{0}/progress/{1}", workflowId, resource);
         }
     }
 }
